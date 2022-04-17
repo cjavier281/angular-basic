291dd2cdc02dd0e3c4f582831168083aaa2d4cd9
INTERRAPIDISIMO\carlosjmarentesr <cjavier28@gmail.com>
2022-04-17 16:50:01 -0500

    tests
    Signed-off-by: INTERRAPIDISIMO\carlosjmarentesr <cjavier28@gmail.com>

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje{
+  nombre:string;
+  poder:number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent  {
+
+nuevo:Personaje={
+nombre:"Trunks",
+poder:8
+}
 
 agregar():void{
   // event.preventDefault();
