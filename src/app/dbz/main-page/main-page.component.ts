2c3ba6b92c30ee4aa64dd03829fb92aad0be8079
INTERRAPIDISIMO\carlosjmarentesr <cjavier28@gmail.com>
2022-04-22 11:09:32 -0500

    Se agrega paid  y  se concatenan data

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,17 +11,40 @@ interface Personaje{
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent  {
-
+personajes:Personaje[]=
+[
+  {
+    nombre:"Test1",
+    poder:500
+  },
+  {
+    nombre:"Test2",
+    poder:50000
+  },
+]
 nuevo:Personaje={
-nombre:"Trunks",
-poder:8
+nombre:"",  
+poder:0
 }
 cambiarNombre(event:any){
   console.log(event.target.value);
 }
 agregar():void{
+  if(this.nuevo.nombre.trim().length===0){
+    return;
+  }
+
+  if(this.nuevo.poder===0){
+    return;
+  }
+  this.personajes.push(this.nuevo);
   // event.preventDefault();
+
   console.log(this.nuevo);
+  this.nuevo={
+    nombre:"",
+    poder:0
+  }
 }
   }
   
